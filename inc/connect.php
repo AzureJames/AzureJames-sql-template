2ae1df663f3f4afc8baa47b8e4e821dbf9f42659
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-09-19 09:11:58 -0600

    fixed excpt path

--- inc/connect.php
+++ inc/connect.php
@@ -1,5 +1,5 @@
 <?php 
-$conn = new mysqli("localhost", "username", "password", "database");
+$conn = new mysqli("localhost", "agallaghermicha1", "HIwViS4BbKBFrsx", "agallaghermicha1");
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 
 if (!defined("BASE_URL")) {
